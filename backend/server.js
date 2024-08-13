7ae6c4c3700acf388ea99592fa6c74ec85e992ba
judd.gnizama <juddmaverick@gmail.com>
2024-08-13 16:33:10 +0800

    changed port

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import testRouter from "./routes/test.route.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 const NODE_ENV = process.env.NODE_ENV;
 
 const app = express();
@@ -36,6 +36,7 @@ app.use("/api/test", testRouter);
 try {
   await mongoose.connect(process.env.MONGO_URI, { dbName: "cc_db" });
   console.log("Connected to database successfully.");
+  console.log(PORT);
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
