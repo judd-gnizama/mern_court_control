26e91f32ec9399503134e59359cd49add813d5e2
judd.gnizama <juddmaverick@gmail.com>
2024-08-15 12:40:25 +0800

    modified-server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import { echoRouter } from "./routes/echoRouter.js";
 import testRouter from "./routes/test.route.js";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
@@ -12,15 +14,22 @@ const NODE_ENV = process.env.NODE_ENV;
 
 const app = express();
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 // Middlewares
 if (NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "./frontend/dist")));
 
 // Routes
 const version = "v1";
 app.use(`/api/${version}/echo`, echoRouter);
+
+app.get("/", (req, res) => {
+  res.json({ path: path.resolve(__dirname, "./frontend/dist") });
+});
 
 // START EDIT - kyle - 8/10/24
 app.use("/api/hello", (req, res) => {
@@ -31,6 +40,10 @@ app.use("/api/hello", (req, res) => {
 // START ADD - kyle - 8/10/24
 app.use("/api/test", testRouter);
 // END ADD - kyle - 8/10/24
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./frontend/dist", "index.html"));
+});
 
 // Connect to Database
 try {
