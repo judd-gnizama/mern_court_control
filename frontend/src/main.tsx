ee4bedfc8783ecdddf572255178901d6735c3be0
judd.gnizama <juddmaverick@gmail.com>
2024-08-15 16:14:32 +0800

    fixed lint error

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,4 @@
-import { createContext, StrictMode, useContext, useState } from "react";
+import { createContext, StrictMode, useContext } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
